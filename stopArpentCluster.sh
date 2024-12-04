aaaea7ccd32117c46b80c3196614b809b40147a9
Ornellah <ornella.helvas@outlook.fr>
2024-12-04 22:37:58 +0100

    add kill kafka and zookeeper server

--- stopArpentCluster.sh
+++ stopArpentCluster.sh
@@ -1,5 +1,12 @@
 # delete topics
 konsole --workdir "/opt/kafka" -e awk -F';' '{ system ("./bin/kafka-topics.sh --bootstrap-server localhost:9092 --delete --topic=\"" $1 "\"") }' ~/Documents/Git\ project/SparkKafka/topics.txt
 
+# kill kafka server
+konsole --workdir "/opt/kafka" -e "bash ./bin/kafka-server-stop.sh"
+
+# kill zookeeper server
+konsole --workdir "/opt/kafka" -e "bash ./bin/zookeeper-server-stop.sh"
+
+
 # kill all konsole
-killall -9 konsole
+# killall -9 konsole
